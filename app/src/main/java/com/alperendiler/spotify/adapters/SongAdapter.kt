0340279aed66416dd643450d6ed29daa8721e586
alperendiler <alperendiler1@gmail.com>
2022-06-06 05:50:36 +0300

    son

---
+++ app/src/main/java/com/alperendiler/spotify/adapters/SongAdapter.kt
@@ -0,0 +1,47 @@
+package com.alperendiler.spotify.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.alperendiler.spotify.R
+import com.alperendiler.spotify.data.entities.Song
+import com.bumptech.glide.RequestManager
+import kotlinx.android.synthetic.main.list_item.view.*
+import java.util.*
+import javax.inject.Inject
+
+class SongAdapter @Inject constructor(
+    private val glide: RequestManager
+) : BaseSongAdapter(R.layout.list_item) {
+
+    override val differ = AsyncListDiffer(this, diffCallback)
+
+
+    override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
+        val song = songs[position]
+        holder.itemView.apply {
+            tvPrimary.text = song.title
+            tvSecondary.text = song.subtitle
+            glide.load(song.imageUrl).into(ivItemImage)
+
+            setOnClickListener {
+                onItemClickListener?.let { click ->
+                    click(song)
+                }
+            }
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
